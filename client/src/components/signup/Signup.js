a881e62aa54285bc72cd92ce53c3a7c16c618140
Seungwoo Yun <yoooonseungwoo@gmail.com>
2023-11-20 18:43:37 +0800

    working on signup

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
-import { AuthContext } from "../application/store/AuthContext"
-import { useNavigate, Link } from 'react-router-dom'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../application/store/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import styled, { createGlobalStyle } from "styled-components";
 import golfimage from "../../svgs/golfimage.svg";
@@ -9,14 +11,13 @@ import "./Signup.css";
 import { Form, Col, Row, Container, Button } from "react-bootstrap";
 import GoogleLogin from "../application/GoogleLogin";
 
-// 컨테이너에 백그란운드 + 컨텐츠 두개 들어가고, 백그라운드를 4 컨테이너 6으로 놓고. 백그라운드에 꾸며주면 됨
 const Background = styled.div`
   background-color: #1b4607;
   display: flex;
   height: 100vh;
 `;
 
-const GlobalStyle = createGlobalStyle` // 배경꽉채우기
+const GlobalStyle = createGlobalStyle`
   body{
     padding: 0;
     margin: 0;
@@ -45,51 +46,48 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [ispro, setIsPro] = useState(false);
-  const [error, setError] = useState(""); // Add state for displaying a single error message
+  const [birth_date, setBirthDate] = useState("");
+  const [gender, setGender] = useState("");
+  const [isPro, setIsPro] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const validatePassword = (password) => {
     const regex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
     return regex.test(password);
-  }
-
+  };
 
   const Register = async (event) => {
     event.preventDefault();
-    setError("")
+    setError("");
 
-    // 최소4글자, 영문, 숫자 포함 regex
-    if (validatePassword(password) && password === confirmPassword) {
+    if (
+      validatePassword(password) &&
+      password === confirmPassword &&
+      birth_date &&
+      (gender === "male" || gender === "female" || gender === "other")
+    ) {
       const userData = {
         username,
         email,
         password,
         confirmPassword,
+        birth_date: birth_date.toISOString().split("T")[0],
+        gender,
+        isPro
       };
 
       try {
-        let result;
-        if (ispro) {
-          result = await axios.post("http://localhost:3000/signup", userData);
-          console.log(result.data.message); // 여기에 추가
-          navigate("/")
-        } else {
-          result = await axios.post("http://localhost:3000/signup", userData);
-          console.log(result.data.message); // 여기에 추가
-          navigate("/")
-        }
-        console.log(result.data);
+        const result = await axios.post("http://localhost:3000/signup", userData);
+        console.log(result.data.message);
+        navigate("/");
       } catch (error) {
         setError(error.response.data.message);
       }
     } else {
-      if (!validatePassword(password) || password !== confirmPassword) {
-        setError("Please check your input fields."); // Set a generic error message
-      }
+      setError("Please check your input fields.");
     }
   };
-
 
   return (
     <>
@@ -127,7 +125,7 @@ const Signup = () => {
                   id="user"
                   onClick={() => setIsPro(false)}
                   inline="true"
-                  className={!ispro ? "mr-3 isProButton_clicks" : "mr-3"}
+                  className={!isPro ? "mr-3 isProButton_clicks" : "mr-3"}
                   style={{
                     backgroundColor: "#F3F3F3",
                     height: "60px",
@@ -141,7 +139,7 @@ const Signup = () => {
                   value="Pro"
                   name="isPro"
                   id="pro"
-                  className={ispro ? "isProButton_clicks" : ""}
+                  className={isPro ? "isProButton_clicks" : ""}
                   onClick={() => setIsPro(true)}
                   inline="true"
                   style={{
@@ -185,7 +183,10 @@ const Signup = () => {
                   type="password"
                   placeholder="Password"
                   value={password}
-                  onChange={(e) => { setPassword(e.target.value); setError(""); }}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setError("");
+                  }}
                   className="mb-3 place_holder"
                   style={{
                     backgroundColor: "#F3F3F3",
@@ -193,14 +194,16 @@ const Signup = () => {
                     borderRadius: "18px",
                   }}
                 />
-
               </Form.Group>
               <Form.Group>
                 <Form.Control
                   type="password"
                   placeholder="Confirm Password"
                   value={confirmPassword}
-                  onChange={(e) => { setConfirmPassword(e.target.value); setError(""); }}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    setError("");
+                  }}
                   className="mb-3 place_holder"
                   style={{
                     backgroundColor: "#F3F3F3",
@@ -209,8 +212,44 @@ const Signup = () => {
                   }}
                 />
               </Form.Group>
+              <Form.Group>
+                <DatePicker
+                  selected={birth_date}
+                  placeholderText="Birth date"
+                  onChange={(date) => setBirthDate(date)}
+                  className="mb-3 form-control place_holder"
+                  dateFormat="yyyy-MM-dd"
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  className="mb-3 place_holder"
+                  style={{
+                    backgroundColor: "#F3F3F3",
+                    height: "50px",
+                    borderRadius: "18px",
+                  }}
+                >
+                  <option value="" disabled>
+                    Select Gender
+                  </option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </Form.Control>
+              </Form.Group>
               {error && (
-                <p style={{ color: "red", fontSize: "14px", textAlign: "center", margin: "10px 0" }}>
+                <p
+                  style={{
+                    color: "red",
+                    fontSize: "14px",
+                    textAlign: "center",
+                    margin: "10px 0",
+                  }}
+                >
                   {error}
                 </p>
               )}
@@ -241,3 +280,250 @@ const Signup = () => {
 };
 
 export default Signup;
+
+
+
+
+// import React, { useState, useContext } from "react";
+// import { AuthContext } from "../application/store/AuthContext"
+// import { useNavigate, Link } from 'react-router-dom'
+// import axios from "axios";
+// import styled, { createGlobalStyle } from "styled-components";
+// import golfimage from "../../svgs/golfimage.svg";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./Signup.css";
+// import { Form, Col, Row, Container, Button } from "react-bootstrap";
+// import GoogleLogin from "../application/GoogleLogin";
+
+// // 컨테이너에 백그란운드 + 컨텐츠 두개 들어가고, 백그라운드를 4 컨테이너 6으로 놓고. 백그라운드에 꾸며주면 됨
+// const Background = styled.div`
+//   background-color: #1b4607;
+//   display: flex;
+//   height: 100vh;
+// `;
+
+// const GlobalStyle = createGlobalStyle` // 배경꽉채우기
+//   body{
+//     padding: 0;
+//     margin: 0;
+//   }
+// `;
+
+// const Background_Content = styled.div`
+//   position: relative;
+//   width: 80%;
+//   left: 150px;
+//   height: 75vh;
+//   top: 180px;
+//   font-family: "DM Sans";
+//   font-weight: 700;
+// `;
+
+// const Golf_image = styled.img`
+//   position: relative;
+//   width: 80%;
+//   left: 120px;
+// `;
+
+// const Signup = () => {
+//   const { isLoggedIn, login, logout } = useContext(AuthContext);
+//   const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+//   const [isPro, setIsPro] = useState(false);
+//   const [error, setError] = useState(""); // Add state for displaying a single error message
+//   const navigate = useNavigate();
+
+//   const validatePassword = (password) => {
+//     const regex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
+//     return regex.test(password);
+//   }
+
+
+//   const Register = async (event) => {
+//     event.preventDefault();
+//     setError("")
+
+//     // 최소4글자, 영문, 숫자 포함 regex
+//     if (validatePassword(password) && password === confirmPassword) {
+//       const userData = {
+//         username,
+//         email,
+//         password,
+//         confirmPassword,
+//       };
+
+//       try {
+//         let result;
+//         if (isPro) {
+//           result = await axios.post("http://localhost:3000/signup", userData);
+//           console.log(result.data.message); // 여기에 추가
+//           navigate("/")
+//         } else {
+//           result = await axios.post("http://localhost:3000/signup", userData);
+//           console.log(result.data.message); // 여기에 추가
+//           navigate("/")
+//         }
+//         console.log(result.data);
+//       } catch (error) {
+//         setError(error.response.data.message);
+//       }
+//     } else {
+//       if (!validatePassword(password) || password !== confirmPassword) {
+//         setError("Please check your input fields."); // Set a generic error message
+//       }
+//     }
+//   };
+
+
+//   return (
+//     <>
+//       <Container fluid>
+//         <GlobalStyle />
+//         <Row>
+//           <Col xl={4} className="fill_bg">
+//             <Background>
+//               <Background_Content>
+//                 <p
+//                   style={{
+//                     color: "#A4A3A3",
+//                     fontSize: "20px",
+//                     fontWeight: "200",
+//                   }}
+//                 >
+//                   welcome
+//                 </p>
+//                 <h2 style={{ color: "white", fontSize: "40px" }}>Sign Up</h2>
+//                 <Golf_image src={golfimage} />
+//               </Background_Content>
+//             </Background>
+//           </Col>
+//           <Col
+//             xl={8}
+//             className="d-flex align-items-center justify-content-center"
+//           >
+//             <Form style={{ width: "40%" }}>
+//               <Form.Group className="d-flex justify-content-between mb-3">
+//                 <Form.Control
+//                   type="button"
+//                   value="User"
+//                   label="User"
+//                   name="isPro"
+//                   id="user"
+//                   onClick={() => setIsPro(false)}
+//                   inline="true"
+//                   className={!isPro ? "mr-3 isProButton_clicks" : "mr-3"}
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "60px",
+//                     width: "50%",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+//                 <Form.Control
+//                   type="button"
+//                   label="Pro"
+//                   value="Pro"
+//                   name="isPro"
+//                   id="pro"
+//                   className={isPro ? "isProButton_clicks" : ""}
+//                   onClick={() => setIsPro(true)}
+//                   inline="true"
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "60px",
+//                     width: "50%",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+//               </Form.Group>
+//               <Form.Group>
+//                 <Form.Control
+//                   type="text"
+//                   placeholder="Username"
+//                   value={username}
+//                   onChange={(e) => setUsername(e.target.value)}
+//                   className="mb-3 place_holder"
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "50px",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+//               </Form.Group>
+//               <Form.Group>
+//                 <Form.Control
+//                   type="email"
+//                   placeholder="Email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   className="mb-3 place_holder"
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "50px",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+//               </Form.Group>
+//               <Form.Group>
+//                 <Form.Control
+//                   type="password"
+//                   placeholder="Password"
+//                   value={password}
+//                   onChange={(e) => { setPassword(e.target.value); setError(""); }}
+//                   className="mb-3 place_holder"
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "50px",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+
+//               </Form.Group>
+//               <Form.Group>
+//                 <Form.Control
+//                   type="password"
+//                   placeholder="Confirm Password"
+//                   value={confirmPassword}
+//                   onChange={(e) => { setConfirmPassword(e.target.value); setError(""); }}
+//                   className="mb-3 place_holder"
+//                   style={{
+//                     backgroundColor: "#F3F3F3",
+//                     height: "50px",
+//                     borderRadius: "18px",
+//                   }}
+//                 />
+//               </Form.Group>
+//               {error && (
+//                 <p style={{ color: "red", fontSize: "14px", textAlign: "center", margin: "10px 0" }}>
+//                   {error}
+//                 </p>
+//               )}
+//               <div style={{ display: "flex", justifyContent: "center" }}>
+//                 <Button
+//                   variant="success"
+//                   onClick={Register}
+//                   style={{
+//                     width: "80%",
+//                     height: "45px",
+//                     backgroundColor: "#1B4607",
+//                   }}
+//                 >
+//                   Sign Up
+//                 </Button>
+//               </div>
+
+//               <br />
+//               <GoogleLogin />
+//               Already have an account? <Link to="/signin">Sign in</Link>
+//               <br />
+//             </Form>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// };
+
+// export default Signup;
--- server/app/src/routes/user/user.ctrl.js
+++ server/app/src/routes/user/user.ctrl.js
@@ -1,5 +1,5 @@
 "use strict";
-const {User} = require('../../models/model');
+const { User } = require('../../models/model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -20,10 +20,10 @@ const api = {
         try {
             const user = await User.findOne({ username: req.body.username });
             if (!user) return res.status(400).json({ message: "아이디를 찾을 수 없습니다." });
-    
+
             const validPassword = await bcrypt.compare(req.body.password, user.password);
             if (!validPassword) return res.status(400).json({ message: "잘못된 비밀번호입니다." });
-    
+
             const token = jwt.sign({ _id: user._id }, 'pmatch');
             res.json({ token });
         } catch (error) {
@@ -33,39 +33,42 @@ const api = {
     signup: async (req, res) => {
         try {
             // 입력 검증
-            const { username, email, password, confirmPassword, birthDate, gender, isPro } = req.body;
-    
+            const { username, email, password, confirmPassword, birth_date, gender, isPro } = req.body;
+
             // 이메일 형식 검사
             const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
             if (!emailRegex.test(email)) {
                 return res.status(400).json({ message: "유효하지 않은 이메일 형식입니다." });
             }
-    
+
             // 비밀번호 유효성 검사: 영문 + 숫자, 최소 6글자
-            const passwordRegex =  /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
+            const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
             if (!passwordRegex.test(password)) {
                 return res.status(400).json({ message: "비밀번호는 영문과 숫자를 포함하여 최소 6글자 이상이어야 합니다." });
             }
-    
+
             // 비밀번호 일치 검사
             if (password !== confirmPassword) {
                 return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
             }
-    
+
             // 생년월일 형식 검사: YYYY-MM-DD
+
+            // 동민님 이부분 string으로 넘어오면 regex 검사하고, 밑에서 Date로 타입 바꿔줘야할 거 같아요
             const birthDateRegex = /^\d{4}-\d{2}-\d{2}$/;
-            if (!birthDateRegex.test(birthDate)) {
+            if (!birthDateRegex.test(birth_date)) {
                 return res.status(400).json({ message: "유효하지 않은 생년월일 형식입니다. (YYYY-MM-DD)" });
             }
-    
+            const birthDateObject = new Date(birth_date)
+
             // 성별 검사: 'male', 'female', 'other'
             if (!['male', 'female', 'other'].includes(gender)) {
                 return res.status(400).json({ message: "유효하지 않은 성별입니다. (male, female, other 중 선택)" });
             }
-    
+
             // isPro 검사: 체크박스로 넘어온 값 변환
             const isProBoolean = isPro === 'true';
-    
+
             // 중복 검사
             let user = await User.findOne({ username });
             if (user) {
@@ -75,27 +78,28 @@ const api = {
             if (user) {
                 return res.status(400).json({ message: "이미 존재하는 이메일 주소입니다." });
             }
-    
+
             // 비밀번호 해싱
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
-    
+
             // 데이터 저장
             user = new User({
                 username,
                 email,
                 password: hashedPassword,
-                birthDate,
+                birth_date: birthDateObject,
+                // birthDate
                 gender,
                 isPro: isProBoolean
             });
             await user.save();
-    
-            res.render('user/login', { message: "회원가입 성공! 로그인하세요." });
+            res.status(200).json({ message: "회원가입 성공! 로그인하세요." });
+            // res.render('user/login', { message: "회원가입 성공! 로그인하세요." });
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
-    },    
+    },
 }
 
 module.exports = {
