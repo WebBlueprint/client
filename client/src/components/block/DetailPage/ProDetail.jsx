641ff8c130d6619d442e37a6a368deaef81cb7ec
Minji Kim <110662771+sloane323@users.noreply.github.com>
2024-03-12 05:41:54 +0000

    pro detail

--- client/src/components/block/DetailPage/ProDetail.jsx
+++ client/src/components/block/DetailPage/ProDetail.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Detail.module.css";
 import proboxData from "./ProDetail.json";
+import { ReactComponent as Mypro } from "./MyPro.svg";
+import { ReactComponent as AddPro } from "./AddPro.svg";
+import Slider from "react-slick";
+import ProPhoto from "./ProPhoto";
+
+
 
 const ProDetail = () => {
-  const [selectedProId, setSelectedProId] = useState(1); // 기본값으로 proid 1 설정
+  const [selectedProId, setSelectedProId] = useState(1);
   const [selectedPro, setSelectedPro] = useState(null);
   const [activeTab, setActiveTab] = useState('Careers');
+  const [likedPros, setLikedPros] = useState([]);
+
+
 
   useEffect(() => {
-    // proid에 해당하는 프로 정보 가져오기
     const proInfo = proboxData.find(pro => pro.Proid === selectedProId);
     setSelectedPro(proInfo);
   }, [selectedProId]);
@@ -17,89 +25,120 @@ const ProDetail = () => {
     setActiveTab(tabName);
   };
 
+  const handleLikeClick = () => {
+    // 선택된 프로의 Like 상태를 토글
+    setSelectedPro(prevPro => ({
+      ...prevPro,
+      Like: prevPro.Like === 1 ? 0 : 1
+    }));
+  };
+
   return (
     <div className={styles.appsin}>
       <div className={styles.cover}>
         {selectedPro && (
           <>
-            <h4>Pro Profile</h4>
+             <div> Pro Detail </div>
             <div className={styles.probox1}>
-              <div>
-                <img src={selectedPro.image} alt="프로 사진" />
+              <div className={styles.imagebox}>
+                <img className={styles.imagethumbnail} src={selectedPro.image} alt="프로 사진" />
               </div>
-              <div>
-                <div>{selectedPro.Proname}</div>
-                <div>별점: {selectedPro.StarAvarage}</div>
-                <div>해시태그: {selectedPro.Hastag.map(tag => `#${tag}`).join(", ")}</div>
-                <button>Chat 버튼</button>
+              <div className={styles.probox3}>
+
+                <div  className={styles.likeButtons}> 
+             <div> <h3>{selectedPro.Proname}</h3>  </div> 
+             
+               <div className={styles.likeButton} onClick={handleLikeClick}>
+                  {selectedPro.Like === 1 ? (
+                    <>
+                      <Mypro />
+                      <span className={styles.likeButtontext}>Added</span>
+                    </>
+                  ) : (
+                    <>
+                      <AddPro />
+                      <span className={styles.likeButtontext}>Add My Pro</span>
+                    </>
+                  )}
+                </div>  
+
+
+                 </div> 
+                <div>Review Rate {selectedPro.StarAvarage}</div>
+                <div className={styles.hastag}><b>{selectedPro.Hastag.map(tag => `#${tag}`).join(", ")}</b></div>
+                <div className={styles.btnchat}>Chat Now</div>
+              
               </div>
             </div>
 
-            <div className={styles.probox2}> 
-            <div className={styles.tabs}>
-              <div className={`${styles.tab} ${activeTab === 'Careers' ? styles.active : ''}`} onClick={() => handleTabClick('Careers')}>Careers</div>
-              <div className={`${styles.tab} ${activeTab === 'Curriculum' ? styles.active : ''}`} onClick={() => handleTabClick('Curriculum')}>Curriculum</div>
-              <div className={`${styles.tab} ${activeTab === 'Reviews' ? styles.active : ''}`} onClick={() => handleTabClick('Reviews')}>Reviews</div>
-            </div>
-            <div className={styles.tabContent}>
-              {activeTab === 'Careers' && <p>{selectedPro.Careers}</p>}
-              {activeTab === 'Curriculum' && <p>{selectedPro.Curriculum}</p>}
-              {activeTab === 'Reviews' && (
-                <div className={styles.golfRange}>
-                  {selectedPro.Reviews.map(review => (
-                    <div className={styles.golfRangebox} > 
-                    <div key={review.id}>
-                      <p>{review.author}</p>
-                      <p>Rating: {review.rating}</p>
-                      <p>{review.comment}</p>
-                    </div> </div> 
-                  ))}
-                </div>
-              )}
-            </div>
-            <hr /> 
+            <div className={styles.probox2}>
+              <div className={styles.tabs}>
+                <div className={`${styles.tab} ${activeTab === 'Careers' ? styles.active : ''}`} onClick={() => handleTabClick('Careers')}>Careers</div>
+                <div className={`${styles.tab} ${activeTab === 'Curriculum' ? styles.active : ''}`} onClick={() => handleTabClick('Curriculum')}>Curriculum</div>
+                <div className={`${styles.tab} ${activeTab === 'Reviews' ? styles.active : ''}`} onClick={() => handleTabClick('Reviews')}>Reviews</div>
+              </div>
+              <div className={styles.tabContent}>
+                {activeTab === 'Careers' && <p>{selectedPro.Careers}</p>}
+                {activeTab === 'Curriculum' && <p>{selectedPro.Curriculum}</p>}
+                {activeTab === 'Reviews' && (
+                  <div className={styles.golfRange}>
+                    {selectedPro.Reviews.map(review => (
+                      <div className={styles.golfRangebox} key={review.id}>
+                        <b>{review.author}</b>
+                        <p>Rating: {review.rating}</p>
+                        <p>{review.comment}</p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <br />
+              <div  className={styles.tabs}> Photos </div>
 
-            <div className={styles.photos}>
-              {selectedPro.Photos.map((photo, index) => (
-                <img key={index} src={photo} alt={`Photo ${index + 1}`} className={styles.photo} />
-              ))}
-            </div>
-            <hr /> 
+              {/*
+              <div className={styles.photos}>
+                {selectedPro.Photos.map((photo, index) => (
+                  <div key={index} className={styles.photoWrapper}>
+                    <img src={photo} alt={`Photo ${index + 1}`} className={styles.photo} />
+                  </div>
+                ))}
+              </div> // */} 
+          <ProPhoto />
 
-    
-              <h3>Golf Range</h3> 
+              <hr />
+
+              <h3>Golf Range</h3>
               <div className={styles.golfRange}>
-              {Array.isArray(selectedPro["Golf Range"]) ? (
-                selectedPro["Golf Range"].map((range, idx) => (
-                  <div key={idx} className={styles.golfRangebox} >
-                    <p><strong>Name:</strong> {range.name}</p>
-                    <p><strong>Address:</strong> {range.Address}</p>
-                    <p><strong>Range ID:</strong> {range.rangeID}</p>
+                {Array.isArray(selectedPro["Golf Range"]) ? (
+                  selectedPro["Golf Range"].map((range, idx) => (
+                    <div key={idx} className={styles.golfRangebox}>
+                      <p> {range.name}</p>
+                      <p>{range.Address}</p>
+                      <div className={styles.rangePhotos}>
+                        {range.photo.map((photo, index) => (
+                          <img key={index} src={photo} alt={`Range Photo ${index + 1}`} className={styles.rangePhoto} />
+                        ))}
+                      </div>
+                    </div>
+                    
+                  ))
+                ) : (
+                  <div>
+                    <p><strong>Name:</strong> {selectedPro["Golf Range"].name}</p>
+                    <p><strong>Address:</strong> {selectedPro["Golf Range"].Address}</p>
+                    <p><strong>Range ID:</strong> {selectedPro["Golf Range"].rangeID}</p>
                     <div className={styles.rangePhotos}>
-                      {range.photo.map((photo, index) => (
+                      {selectedPro["Golf Range"].photo.map((photo, index) => (
                         <img key={index} src={photo} alt={`Range Photo ${index + 1}`} className={styles.rangePhoto} />
                       ))}
                     </div>
                   </div>
-                ))
-              ) : (
-                <div>
-                  <p><strong>Name:</strong> {selectedPro["Golf Range"].name}</p>
-                  <p><strong>Address:</strong> {selectedPro["Golf Range"].Address}</p>
-                  <p><strong>Range ID:</strong> {selectedPro["Golf Range"].rangeID}</p>
-                  <div className={styles.rangePhotos}>
-                    {selectedPro["Golf Range"].photo.map((photo, index) => (
-                      <img key={index} src={photo} alt={`Range Photo ${index + 1}`} className={styles.rangePhoto} />
-                    ))}
-                  </div>
-                </div>
-                    
-              )}   </div>
+                )}
+              </div>
             </div>
-           </>
+          </>
         )}
-     </div> 
-
+      </div>
     </div>
   );
 };
---
+++ client/src/components/block/DetailPage/ProPhoto.jsx
@@ -0,0 +1,12 @@
+
+
+const ProPhoto = () => {
+ 
+  return (
+    <div> 
+        여기에 사진들이 담기면 되는데 
+        </div>
+  );
+};
+
+export default ProPhoto;
